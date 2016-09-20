5daeec768b4b834cc97958213a9e787dd9090d80
Steve Wagner <swagner@swagner-mac.local>
2016-09-20 07:27:52 -0700

    Adding minification and uglification.

--- dist/n-app-conf.js
+++ dist/n-app-conf.js
@@ -1,50 +1,58 @@
 "use strict";
-const fs = require("fs");
-const extend = require("extend");
-const util_1 = require("util");
-class Options {
-    constructor(filename) {
+var fs = require("fs");
+var extend = require("extend");
+var util_1 = require("util");
+var Options = (function () {
+    function Options(filename) {
         this.filename = filename;
     }
-}
+    return Options;
+}());
 exports.Options = Options;
-class Settings {
-    constructor(options) {
+var Settings = (function () {
+    function Settings(options) {
         this.options = options;
         this.DEFAULT_HIERARCHY_DELIMITER = '__';
         this._entries = {};
         this.buildSettings();
         this.applyEnvironmentOverrides(this._entries);
     }
-    get entries() {
-        return this._entries;
-    }
-    applyEnvironmentOverrides(settings, previousScope = null) {
-        Object.keys(settings).forEach((key) => {
-            let value = settings[key];
+    Object.defineProperty(Settings.prototype, "entries", {
+        get: function () {
+            return this._entries;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Settings.prototype.applyEnvironmentOverrides = function (settings, previousScope) {
+        var _this = this;
+        if (previousScope === void 0) { previousScope = null; }
+        Object.keys(settings).forEach(function (key) {
+            var value = settings[key];
             if (typeof (value) === 'object') {
-                let scope = previousScope === null ? key : `${previousScope}${this.DEFAULT_HIERARCHY_DELIMITER}${key}`;
-                this.applyEnvironmentOverrides(value, scope);
+                var scope = previousScope === null ? key : "" + previousScope + _this.DEFAULT_HIERARCHY_DELIMITER + key;
+                _this.applyEnvironmentOverrides(value, scope);
             }
             else {
-                this.processKey(key, settings, previousScope);
+                _this.processKey(key, settings, previousScope);
             }
         });
-    }
-    buildSettings() {
-        let fileContent = fs.readFileSync(this.options.filename, "UTF8");
+    };
+    Settings.prototype.buildSettings = function () {
+        var fileContent = fs.readFileSync(this.options.filename, "UTF8");
         extend(true, this._entries, JSON.parse(fileContent));
-    }
-    processKey(key, settings, previousScope) {
-        let envKey = previousScope === null ? key : `${previousScope}${this.DEFAULT_HIERARCHY_DELIMITER}${key}`;
-        let envVal = process.env[envKey];
+    };
+    Settings.prototype.processKey = function (key, settings, previousScope) {
+        var envKey = previousScope === null ? key : "" + previousScope + this.DEFAULT_HIERARCHY_DELIMITER + key;
+        var envVal = process.env[envKey];
         if (!util_1.isNullOrUndefined(envVal)) {
             settings[key] = envVal;
         }
         else {
             settings[key] = settings[key];
         }
-    }
-}
+    };
+    return Settings;
+}());
 exports.Settings = Settings;
 //# sourceMappingURL=n-app-conf.js.map
